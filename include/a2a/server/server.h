563f4b550a1ddd730095d2c0d5caaa199bd25123
MisterVVP <mistervvp@live.ru>
2026-05-05 22:21:11 +0300

    fix code style

--- include/a2a/server/server.h
+++ include/a2a/server/server.h
@@ -36,16 +36,8 @@ struct ListTasksRequest final {
   ListTasksRequest(std::size_t page_size_value, std::string page_token_value)
       : page_size(page_size_value), page_token(std::move(page_token_value)) {}
 
-  ListTasksRequest(const ListTasksRequest& other)
-      : page_size(other.page_size), page_token(other.page_token) {}
-
-  ListTasksRequest& operator=(const ListTasksRequest& other) {
-    if (this != &other) {
-      page_size = other.page_size;
-      page_token = other.page_token;
-    }
-    return *this;
-  }
+  ListTasksRequest(const ListTasksRequest&) = default;
+  ListTasksRequest& operator=(const ListTasksRequest&) = default;
 
   ListTasksRequest(ListTasksRequest&&) noexcept = default;
   ListTasksRequest& operator=(ListTasksRequest&&) noexcept = default;
