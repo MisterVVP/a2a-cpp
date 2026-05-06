07cb9f687a5e6f55aa67fe1692375cd43f832121
Vladimir Pavlov <43521651+MisterVVP@users.noreply.github.com>
2026-05-06 15:45:48 +0300

    Fix analyzer false positive in ListTasksRequest copy semantics

--- include/a2a/server/server.h
+++ include/a2a/server/server.h
@@ -35,6 +35,20 @@ struct ListTasksRequest final {
 
   ListTasksRequest(std::size_t page_size_value, std::string page_token_value)
       : page_size(page_size_value), page_token(std::move(page_token_value)) {}
+
+  ListTasksRequest(const ListTasksRequest& other)
+      : page_size(other.page_size), page_token(other.page_token) {}
+
+  ListTasksRequest& operator=(const ListTasksRequest& other) {
+    if (this != &other) {
+      page_size = other.page_size;
+      page_token = other.page_token;
+    }
+    return *this;
+  }
+
+  ListTasksRequest(ListTasksRequest&&) noexcept = default;
+  ListTasksRequest& operator=(ListTasksRequest&&) noexcept = default;
 };
 
 struct ListTasksResponse final {
