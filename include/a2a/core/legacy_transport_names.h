bddc31128bbd14e0f8f5ce8bce0b6b65b6f84b7e
Vladimir Pavlov <mistervvp@outlook.com>
2026-05-14 20:01:43 +0300

    small fixes

--- include/a2a/core/legacy_transport_names.h
+++ include/a2a/core/legacy_transport_names.h
@@ -4,9 +4,6 @@
 
 namespace a2a::core::legacy_transport_names {
 
-inline constexpr std::string_view kJsonRpc = "jsonrpc";
-inline constexpr std::string_view kRest = "rest";
-inline constexpr std::string_view kGrpc = "grpc";
 inline constexpr std::string_view kEndpointField = "endpoint";
 inline constexpr std::string_view kPreferredTransportField = "preferredTransport";
 inline constexpr std::string_view kAdditionalInterfacesField = "additionalInterfaces";
