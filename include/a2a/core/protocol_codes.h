27f3b1dc671ed28219f228fdd930a9e5740331e0
Vladimir Pavlov <43521651+MisterVVP@users.noreply.github.com>
2026-05-17 21:44:37 +0300

    fix: add protocol codes for task lifecycle errors

--- include/a2a/core/protocol_codes.h
+++ include/a2a/core/protocol_codes.h
@@ -5,5 +5,7 @@
 namespace a2a::core::protocol_codes {
 
 inline constexpr std::string_view kTaskNotFound = "-32001";
+inline constexpr std::string_view kTaskNotCancelable = "-32002";
+inline constexpr std::string_view kUnsupportedOperation = "-32004";
 
 }  // namespace a2a::core::protocol_codes
