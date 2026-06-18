866d283145cfb52d9732849ad9a3bb4fe290845c
Vladimir Pavlov <43521651+MisterVVP@users.noreply.github.com>
2026-06-18 21:24:25 +0300

    fix: make stream session readiness explicit

--- include/a2a/server/server_stream_session.h
+++ include/a2a/server/server_stream_session.h
@@ -15,7 +15,7 @@ class ServerStreamSession {
   virtual ~ServerStreamSession() = default;
 
   [[nodiscard]] virtual core::Result<std::optional<lf::a2a::v1::StreamResponse>> Next() = 0;
-  [[nodiscard]] virtual bool IsLive() const noexcept { return false; }
+  [[nodiscard]] virtual bool IsLive() const noexcept { return true; }
   virtual void Cancel() noexcept {}
 };
 
