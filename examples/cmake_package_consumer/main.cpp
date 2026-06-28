596dd2050de0d20da77341cc0364eb8274e2e588
Vladimir Pavlov <43521651+MisterVVP@users.noreply.github.com>
2026-06-28 14:21:09 +0300

    Add installed CMake package consumer example source

---
+++ examples/cmake_package_consumer/main.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 Vladimir Pavlov <mistervvp@outlook.com> (https://github.com/MisterVVP)
+
+#include "a2a/core/version.h"
+
+int main() {
+  return a2a::core::Version::HeaderValue().empty() ? 1 : 0;
+}
