9a86db99c18d5c5beceb892790917b1f2ffe8efd
Vladimir Pavlov <43521651+MisterVVP@users.noreply.github.com>
2026-07-03 00:07:54 +0300

    fix: remove deprecated agent card builder shim

--- include/a2a/core/agent_card_builder.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2026 Vladimir Pavlov <mistervvp@outlook.com> (https://github.com/MisterVVP)
-
-#pragma once
-
-#include "a2a/core/agent_card/agent_card_builder.h"
