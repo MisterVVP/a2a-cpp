4b0fae5c5bc3ab17aba9548735b6fad28eec12dc
Vladimir Pavlov <43521651+MisterVVP@users.noreply.github.com>
2026-09-16 09:11:17 +0300

    docs: prepare v0.5.0 release

--- vcpkg-overlay-ports/a2a-cpp/portfile.cmake
+++ vcpkg-overlay-ports/a2a-cpp/portfile.cmake
@@ -9,7 +9,7 @@ vcpkg_check_features(
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL "https://github.com/MisterVVP/a2a-cpp.git"
-    REF v0.4.1
+    REF v0.5.0
 )
 
 vcpkg_cmake_configure(
